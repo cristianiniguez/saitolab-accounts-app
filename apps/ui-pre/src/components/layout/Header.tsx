7a8400df6e23f951cfe055e347f0d27fb07043e0
cristianiniguez <felizpc1@gmail.com>
2022-09-07 22:56:20 -0400

    feature/SA-39 Header scafold created

--- apps/ui-pre/src/components/layout/Header.tsx
+++ apps/ui-pre/src/components/layout/Header.tsx
@@ -1,5 +1,92 @@
+import { ReactNode } from 'react';
+import {
+  Avatar,
+  Box,
+  Button,
+  Flex,
+  HStack,
+  IconButton,
+  Link,
+  Menu,
+  MenuButton,
+  MenuDivider,
+  MenuItem,
+  MenuList,
+  Stack,
+  useColorModeValue,
+  useDisclosure,
+} from '@chakra-ui/react';
+import { CloseIcon, HamburgerIcon } from '@chakra-ui/icons';
+
+const NavLink = ({ children }: { children: ReactNode }) => (
+  <Link
+    _hover={{
+      bg: useColorModeValue('gray.200', 'gray.700'),
+      textDecoration: 'none',
+    }}
+    href={'#'}
+    px={2}
+    py={1}
+    rounded={'md'}
+  >
+    {children}
+  </Link>
+);
+
+const Links = ['Dashboard', 'Projects', 'Team'];
+
 const Header = () => {
-  return <div>Header</div>;
+  const { isOpen, onClose, onOpen } = useDisclosure();
+
+  return (
+    <Box bg={useColorModeValue('gray.100', 'gray.900')} px={4}>
+      <Flex alignItems={'center'} h={16} justifyContent={'space-between'}>
+        <IconButton
+          aria-label={'Open Menu'}
+          display={{ md: 'none' }}
+          icon={isOpen ? <CloseIcon /> : <HamburgerIcon />}
+          onClick={isOpen ? onClose : onOpen}
+          size={'md'}
+        />
+        <HStack alignItems={'center'} spacing={8}>
+          <Box>Logo</Box>
+          <HStack as={'nav'} display={{ base: 'none', md: 'flex' }} spacing={4}>
+            {Links.map((link) => (
+              <NavLink key={link}>{link}</NavLink>
+            ))}
+          </HStack>
+        </HStack>
+        <Flex alignItems={'center'}>
+          <Menu>
+            <MenuButton as={Button} cursor={'pointer'} minW={0} rounded={'full'} variant={'link'}>
+              <Avatar
+                size={'sm'}
+                src={
+                  'https://images.unsplash.com/photo-1493666438817-866a91353ca9?ixlib=rb-0.3.5&q=80&fm=jpg&crop=faces&fit=crop&h=200&w=200&s=b616b2c5b373a80ffc9636ba24f7a4a9'
+                }
+              />
+            </MenuButton>
+            <MenuList>
+              <MenuItem>Link 1</MenuItem>
+              <MenuItem>Link 2</MenuItem>
+              <MenuDivider />
+              <MenuItem>Link 3</MenuItem>
+            </MenuList>
+          </Menu>
+        </Flex>
+      </Flex>
+
+      {isOpen ? (
+        <Box display={{ md: 'none' }} pb={4}>
+          <Stack as={'nav'} spacing={4}>
+            {Links.map((link) => (
+              <NavLink key={link}>{link}</NavLink>
+            ))}
+          </Stack>
+        </Box>
+      ) : null}
+    </Box>
+  );
 };
 
 export default Header;
