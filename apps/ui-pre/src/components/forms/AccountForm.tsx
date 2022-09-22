ee20a3f716b44503ea36bfe3d875be978b21d181
cristianiniguez <felizpc1@gmail.com>
2022-09-22 19:13:42 -0400

    feature/SA-41 Intl strings for account form added

--- apps/ui-pre/src/components/forms/AccountForm.tsx
+++ apps/ui-pre/src/components/forms/AccountForm.tsx
@@ -15,24 +15,29 @@ import { Form, Formik, FormikConfig } from 'formik';
 import { TextInput } from '../inputs';
 // utils
 import * as Yup from 'yup';
+// hooks
+import useFormatMessage from '@/hooks/useFormatMessage';
+import useAppToast from '@/hooks/useAppToast';
 
 type AccountFormConfig = FormikConfig<{ name: string }>;
 
 const AccountFormComponent: AccountFormConfig['component'] = ({ isSubmitting }) => {
+  const t = useFormatMessage();
+
   return (
     <ModalContent as={Form}>
-      <ModalHeader>New Account</ModalHeader>
+      <ModalHeader>{t('account.form.title')}</ModalHeader>
       <ModalCloseButton />
 
       <ModalBody>
         <Stack spacing={4}>
-          <TextInput id='name' label='Account name' name='name' />
+          <TextInput id='name' label={t('account.form.name.label')} name='name' />
         </Stack>
       </ModalBody>
 
       <ModalFooter>
         <Button color='white' colorScheme='green' isLoading={isSubmitting} type='submit'>
-          Save
+          {t('account.form.submitButton.label')}
         </Button>
       </ModalFooter>
     </ModalContent>
@@ -45,13 +50,23 @@ type AccountFormProps = {
 };
 
 const AccountForm: FC<AccountFormProps> = ({ isOpen, onClose }) => {
+  const t = useFormatMessage();
+  const toast = useAppToast();
+
   const getInitialValues = (): AccountFormConfig['initialValues'] => ({ name: '' });
 
   const getValidationSchema = (): AccountFormConfig['validationSchema'] =>
-    Yup.object().shape({ name: Yup.string().required() });
+    Yup.object().shape({ name: Yup.string().required(t('account.form.name.error.required')) });
 
-  const handleSubmit: AccountFormConfig['onSubmit'] = (values) => {
+  const handleSubmit: AccountFormConfig['onSubmit'] = (values, { setSubmitting }) => {
     console.log(values);
+    setSubmitting(false);
+    toast({
+      description: t('account.form.toast.success.description', { name: values.name }),
+      status: 'success',
+    });
+    onClose();
+    // TODO: redirect to account page
   };
 
   return (
