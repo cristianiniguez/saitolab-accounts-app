596f8ad252be14df4ed26301fe4c0f4bad417817
cristianiniguez <felizpc1@gmail.com>
2022-11-14 21:54:11 -0400

    feature/SA-42 Using correct types on data hooks

--- apps/ui-pre/src/hooks/useAccount.ts
+++ apps/ui-pre/src/hooks/useAccount.ts
@@ -1,11 +1,10 @@
-import { doc } from 'firebase/firestore';
+import { doc, DocumentReference } from 'firebase/firestore';
 import { useFirestore, useFirestoreDocData } from 'reactfire';
 
 const useAccount = (accountId: string) => {
   const firestore = useFirestore();
-  const ref = doc(firestore, 'accounts', accountId);
-  const { data, ...rest } = useFirestoreDocData(ref, { idField: 'id' });
-  return { data: data as Account, ...rest };
+  const ref = doc(firestore, 'accounts', accountId) as DocumentReference<Account>;
+  return useFirestoreDocData(ref, { idField: 'id' });
 };
 
 export default useAccount;
--- apps/ui-pre/src/hooks/useMoves.ts
+++ apps/ui-pre/src/hooks/useMoves.ts
@@ -1,11 +1,15 @@
-import { collection } from 'firebase/firestore';
+import { collection, CollectionReference } from 'firebase/firestore';
 import { useFirestore, useFirestoreCollectionData } from 'reactfire';
 
 const useMoves = (account: Account) => {
   const firestore = useFirestore();
-  const movesRef = collection(firestore, 'accounts', account.id, 'moves');
-  const { data, ...rest } = useFirestoreCollectionData(movesRef, { idField: 'id' });
-  return { data: data as Move[], ...rest };
+  const movesRef = collection(
+    firestore,
+    'accounts',
+    account.id,
+    'moves',
+  ) as CollectionReference<Move>;
+  return useFirestoreCollectionData(movesRef, { idField: 'id' });
 };
 
 export default useMoves;
